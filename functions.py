648e619cc7a5661b0a9fe32fe7f80f0fe2dac13f
Miguel <miguel@Miguels-Air.home>
2023-11-14 22:19:07 -0500

    test functions

--- functions.py
+++ functions.py
@@ -20,7 +20,7 @@ def create_assistant(client):
 
     assistant = client.beta.assistants.create(instructions="""
           You are an AI assistant, Ally from Orchid Gym, that has been programmed to help the users to answer questions about Orchid Gym, like pricing information and schedule a visit. Please be concise and give short answers, always in first person. 
-          A document has been provided with information on Orchid Gym prices and general information
+          A series of documents have been provided with information on Orchid Gym prices and general information
           """,
                                               model="gpt-4-1106-preview",
                                               tools=[{
